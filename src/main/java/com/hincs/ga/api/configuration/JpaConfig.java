d66ed26825d1bd0564bc1be2dd520533984924e1
spearkkk <spearkkp@gmail.com>
2022-02-11 22:11:03 +0900

    Hey

---
+++ src/main/java/com/hincs/ga/api/configuration/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.hincs.ga.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+}
---
+++ src/main/java/com/hincs/ga/api/configuration/auth/dto/OAuthAttribute.java
@@ -0,0 +1,67 @@
+package com.hincs.ga.api.configuration.auth.dto;
+
+import com.hincs.ga.api.domain.user.User;
+import com.hincs.ga.api.domain.user.role.Role;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttribute {
+  private Map<String, Object> attributes;
+  private String nameAttributeKey;
+  private String name;
+  private String email;
+  private String picture;
+
+  @Builder(access = AccessLevel.PRIVATE)
+  private OAuthAttribute(final Map<String, Object> attributes, final String nameAttributeKey, final String name,
+                        final String email, final String picture) {
+
+    this.attributes = attributes;
+    this.nameAttributeKey = nameAttributeKey;
+    this.name = name;
+    this.email = email;
+    this.picture = picture;
+  }
+
+  public static OAuthAttribute of(final String registrationId, final String userNameAttributeName, final Map<String, Object> attributes) {
+    if ("naver".equals(registrationId)) {
+      return ofNaver("id", attributes);
+    }
+    return OAuthAttribute.ofGoogle(userNameAttributeName, attributes);
+  }
+
+  private static OAuthAttribute ofNaver(String nameAttributeKey, Map<String, Object> attributes) {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    return OAuthAttribute.builder()
+                         .nameAttributeKey(nameAttributeKey)
+                         .name((String)response.get("name"))
+                         .email((String)response.get("email"))
+                         .picture((String)response.get("profile_image"))
+                         .attributes(response)
+                         .build();
+  }
+
+  private static OAuthAttribute ofGoogle(String nameAttributeKey, Map<String, Object> attributes) {
+    return OAuthAttribute.builder()
+                         .nameAttributeKey(nameAttributeKey)
+                         .name((String)attributes.get("name"))
+                         .email((String)attributes.get("email"))
+                         .picture((String)attributes.get("picture"))
+                         .attributes(attributes)
+                         .build();
+  }
+
+  public User toEntity() {
+    return User.builder()
+               .name(name)
+               .email(email)
+               .picture(picture)
+               .role(Role.GUEST)
+               .build();
+  }
+}
